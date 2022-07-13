bf7319751fa6b4eb3893796da408218a5bb707b0
Robin C. Ladiges <rcl.git@blackpinguin.de>
2022-07-13 02:31:51 +0200

    fix: typo

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ AUTH="Nichole Mattera"
 TAGY="${1:-v0.0.1}"
 VERS="${TAGY:1}"
 
-OUT="./out/athmosphere/contents/0100000000000901"
+OUT="./out/atmosphere/contents/0100000000000901"
 mkdir  -p  $OUT/flags/
 
 export DOCKER_BUILDKIT=1
@@ -28,4 +28,4 @@ cp  ./out/OJDS-NX.nsp      $OUT/exefs.nsp
 ZIP="$PROJ-$TAGY.zip"
 cd ./out/
 rm  -rf  "$ZIP"
-zip  -rm9  "$ZIP"  ./athmosphere/
+zip  -rm9  "$ZIP"  ./atmosphere/
